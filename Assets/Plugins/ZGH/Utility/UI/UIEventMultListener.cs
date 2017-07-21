cd92543b5982ffc3ab888ea7c3572a5f3bd33243
沉淀 <972833647@qq.com>
2017-07-21 17:22:25 +0800

    新增 AssetBundleBrowser 插件

---
+++ Assets/Plugins/ZGH/Utility/UI/UIEventMultListener.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace ZGH.Utility.UI
+{
+    public class UIEventMultListener : MonoBehaviour, IPointerClickHandler, IPointerDownHandler
+    {
+        private int m_layerNum;
+
+        public Action<GameObject> onClick;
+
+        public Action<GameObject> onDown;
+
+        public Action<GameObject> onUp;
+
+        static public UIEventMultListener Get(GameObject go, int num = 1)
+        {
+            UIEventMultListener listener = go.GetComponent<UIEventMultListener>();
+            if (listener == null)
+            {
+                listener = go.AddComponent<UIEventMultListener>();
+                listener.m_layerNum = num;
+            }
+            return listener;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (onClick != null)
+            {
+                onClick.Invoke(this.gameObject);
+                PassEvent(eventData, ExecuteEvents.pointerClickHandler);
+            }
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (onDown != null)
+            {
+                onDown.Invoke(this.gameObject);
+                PassEvent(eventData, ExecuteEvents.pointerDownHandler);
+            }
+        }
+
+        private void PassEvent<T>(PointerEventData data, ExecuteEvents.EventFunction<T> function) where T : IEventSystemHandler
+        {
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(data, results);
+            GameObject current = data.pointerCurrentRaycast.gameObject;
+            int num = 0;
+            for (int i = 0; i < results.Count; i++)
+            {
+                if (current != results[i].gameObject)
+                {
+                    num++;
+                    if (this.m_layerNum == num)
+                    {
+                        return;
+                    }
+                    ExecuteEvents.Execute(results[i].gameObject, data, function);
+                }
+            }
+        }
+    }
+}
